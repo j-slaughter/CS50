0cfae1925f9decbb736a39bfbea686faf593c6d4
Jennifer Slaughter <129903316+j-slaughter@users.noreply.github.com>
2023-08-20 17:22:43 -0700

    Create meow.c

---
+++ meow/meow.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i = 0;
+    while (i < 3)
+    {
+        printf("meow\n");
+        i++;
+    }
+    //or use for loop
+    for (int j = 0; j < 3; j++)
+    {
+        printf("meow\n");
+    }
+}
