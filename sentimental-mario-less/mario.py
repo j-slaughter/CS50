2fa88f0625e54c3d94579466d0466f5a3c4d3113
Jennifer Slaughter <129903316+j-slaughter@users.noreply.github.com>
2023-08-20 17:45:03 -0700

    Create mario.py

---
+++ sentimental-mario-less/mario.py
@@ -0,0 +1,15 @@
+from cs50 import get_int
+
+# Prompt user for height between 1 and 8
+height = 0
+while height < 1 or height > 8:
+    height = get_int("Height: ")
+
+# Build pyramid
+for i in range(height):
+    for j in range(height):
+        if (i + j) < (height - 1):
+            print(" ", end="")
+        else:
+            print("#", end="")
+    print()
